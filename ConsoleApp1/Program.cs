14c630613b73af82430ab3b618e851b73ba4ea45
Hugo Torrico <torricomarquezh@gmail.com>
2024-11-25 21:52:22 -0500

    Capa entidad completa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,43 @@
 ﻿
-
-//InsertarCabecera
-//InsertarDetalle
-
 using Negocio;
+using Entidad;
 
 
 
 void Grabar()
 {
+
     NCabecera nCabecera = new NCabecera();
-    nCabecera.Grabar();
+
+    ECabecera eCabecera = new ECabecera
+    {
+        Cliente = "Juan Torrico",
+        Fecha = DateTime.Now
+    };
+
+    List<EDetalle> eDetalles = new List<EDetalle>();
+    eDetalles.Add(new EDetalle { Cantidad = 1, Precio = 3000, Producto = "PC" });
+    eDetalles.Add(new EDetalle { Cantidad = 2, Precio = 2000, Producto = "TV" });
+    eDetalles.Add(new EDetalle { Cantidad = 4, Precio = 1000, Producto = "Equipo" });
+
+    try
+    {
+        nCabecera.Grabar(eCabecera, eDetalles);
+        Console.WriteLine("Registro exitosamente");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        
+    }
+   
+
+
+
+
 }
 
 
-
-Console.WriteLine("Hello, World!");
+Grabar();
+Console.Read();
 
--- Datos/Constantes.cs
+++ Datos/Constantes.cs
@@ -8,6 +8,8 @@ namespace Datos
 {
     public static class Constantes
     {
-        public static string _connectionString = "";
+        public static string _connectionString = "data source=DESKTOP-BCQFL9J\\SQLEXPRESS;" +
+                  "initial catalog = FacturaDB;  User Id=userPrueba; Pwd=123456;  " +
+                  "TrustServerCertificate=true";
     }
 }
