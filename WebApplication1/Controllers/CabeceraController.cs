bd6ed57d5d79df74f52f640eb5ddc883df928199
Hugo Torrico <torricomarquezh@gmail.com>
2024-11-27 22:18:56 -0500

    Demo Web

---
+++ WebApplication1/Controllers/CabeceraController.cs
@@ -0,0 +1,17 @@
+﻿using Entidad;
+using Microsoft.AspNetCore.Mvc;
+using Negocio;
+
+namespace WebApplication1.Controllers
+{
+    public class CabeceraController : Controller
+    {
+        public IActionResult Index()
+        {
+            NCabecera nCabecera = new NCabecera();
+            List<ECabecera> eCabeceras = nCabecera.Listar("");
+
+            return View(eCabeceras);
+        }
+    }
+}
