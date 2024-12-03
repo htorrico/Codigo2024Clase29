a504954120ce769d1d096760420e9d23b7ba06e9
Hugo Torrico <torricomarquezh@gmail.com>
2024-12-02 22:54:03 -0500

    API Post

--- APIFactura/Controllers/DetalleController.cs
+++ APIFactura/Controllers/DetalleController.cs
@@ -9,10 +9,36 @@ namespace APIFactura.Controllers
     public class DetalleController : ControllerBase
     {
         [HttpPost]
-        public void Insert(EDetalle eDetalle)
-        {            
-            NDetalle nDetalle = new NDetalle();
-            nDetalle.Insertar(eDetalle);
+        public bool Insert(EDetalle eDetalle)
+        {
+            try
+            {
+                NDetalle nDetalle = new NDetalle();
+                nDetalle.Insertar(eDetalle);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }     
+           
+        }
+        [HttpPost]
+        public string Insert2(EDetalle eDetalle)
+        {
+            try
+            {
+                NDetalle nDetalle = new NDetalle();
+                nDetalle.Insertar(eDetalle);
+                return "Registro Exitoso";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+
         }
     }
 }
